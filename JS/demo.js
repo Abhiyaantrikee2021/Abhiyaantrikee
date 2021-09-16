fa3d20dee1209188ca0725dc3ef922f050eb0994
Neha Gamanagatti <75034879+Neha152001@users.noreply.github.com>
2021-09-16 21:07:11 +0530

    Added two more images to slider

--- JS/demo.js
+++ JS/demo.js
@@ -41,5 +41,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
